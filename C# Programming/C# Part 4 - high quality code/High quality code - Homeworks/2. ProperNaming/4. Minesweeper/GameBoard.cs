7495a2d2237d58c0e99f5d8f6c3ed62c0d2c492a
stefanpu <stefanpu13@gmail.com>
2013-04-13 18:09:15 +0300

    "InitializeBoards" method included.
    Together with "InitializeDisplayedBoard()" and
    "InitializeUnderlyingBoard" methods - used for readability.

--- C# Programming/C# Part 4 - high quality code/High quality code - Homeworks/2. ProperNaming/4. Minesweeper/GameBoard.cs
+++ C# Programming/C# Part 4 - high quality code/High quality code - Homeworks/2. ProperNaming/4. Minesweeper/GameBoard.cs
@@ -8,5 +8,47 @@ namespace MinesSweeper
 {
     class GameBoard
     {
+        private char[,] underlyingBoard;
+        private char[,] displayedBoard;        
+
+        public char[,] UnderlyingBoard
+        {
+            get
+            {
+                return this.underlyingBoard;
+            }
+            set
+            {
+                this.underlyingBoard = value;
+            }
+        }
+
+        public char[,] DisplayedBoard
+        {
+            get
+            {
+                return this.displayedBoard;
+            }
+            set
+            {
+                this.displayedBoard = value;
+            }
+        }
+
+        public int BoardRows 
+        { 
+            get 
+            {
+                return 5;
+            } 
+        }
+
+        public int BoardColumns
+        {
+            get
+            {
+                return 10;
+            }
+        }
     }
 }
--- C# Programming/C# Part 4 - high quality code/High quality code - Homeworks/2. ProperNaming/4. Minesweeper/Initializer.cs
+++ C# Programming/C# Part 4 - high quality code/High quality code - Homeworks/2. ProperNaming/4. Minesweeper/Initializer.cs
@@ -8,5 +8,42 @@ namespace MinesSweeper
 {
     class Initializer
     {
+        public static void InitializeBoards(GameBoard gameBoard) 
+        {
+            gameBoard.DisplayedBoard = InitializeDisplayedBoard();
+            gameBoard.UnderlyingBoard = InitializeUnderlyingBoard();
+        }
+
+        private static char[,] InitializeUnderlyingBoard()
+        {
+            return CreateBoard('-');
+        }
+
+        private static char[,] InitializeDisplayedBoard()
+        {
+            return CreateBoard('?');
+        }
+
+        /// <summary>
+        /// Fills a game board with specified field symbol. If the field symbol is question mark
+        /// - '?' this is the displayed board. If the symbol is '-' this is the underlying board.
+        /// </summary>
+        /// <returns>Two dimensional array representing the game board.</returns>
+        private static char[,] CreateBoard(char fieldSymbol)
+        {
+            int boardRows = 5;
+            int boardColumns = 10;
+
+            char[,] board = new char[boardRows, boardColumns];
+
+            for (int row = 0; row < boardRows; row++)
+            {
+                for (int col = 0; col < boardColumns; col++)
+                {
+                    board[row, col] = fieldSymbol;
+                }
+            }
+            return board;
+        }
     }
 }

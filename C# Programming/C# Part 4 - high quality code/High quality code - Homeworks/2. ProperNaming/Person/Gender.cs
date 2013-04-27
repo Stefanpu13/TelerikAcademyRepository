5d76a98c74f2b1898fae38b5ab8484b9a592a311
stefanpu <stefanpu13@gmail.com>
2013-04-27 21:37:33 +0300

    Removing class "Program" and organizing usings

--- C# Programming/C# Part 4 - high quality code/High quality code - Homeworks/2. ProperNaming/Person/Gender.cs
+++ C# Programming/C# Part 4 - high quality code/High quality code - Homeworks/2. ProperNaming/Person/Gender.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Person
+﻿namespace Person
 {
     public enum Gender { Male, Female}
 }
